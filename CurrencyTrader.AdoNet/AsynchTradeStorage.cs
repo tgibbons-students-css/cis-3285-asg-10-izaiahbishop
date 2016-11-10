00c850b3e4df91ab684c2890a56c01c0bcebcc7c
ibishop@css.edu <ibishop@css.edu>
2016-11-10 11:39:23 -0600

    Changes made in class

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    class AsynchTradeStorage : ITradeStorage
+
+    {
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            throw new NotImplementedException();
+        }
+        //The form should be creating an AsynchTradeStorage Object
+
+    }
+}
