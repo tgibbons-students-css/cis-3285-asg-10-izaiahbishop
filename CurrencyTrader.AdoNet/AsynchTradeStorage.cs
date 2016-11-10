108b140aa24976e9fba2d681dbfd03e5e095f5df
ibishop@css.edu <ibishop@css.edu>
2016-11-10 15:40:38 -0600

    Completed Assignment

--- CurrencyTrader.AdoNet/AsynchTradeStorage.cs
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -1,19 +1,31 @@
-﻿using CurrencyTrader.Contracts;
+﻿using CurrencyTrader.AdoNet;
+using CurrencyTrader.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CurrencyTrader.AdoNet
+namespace CurrencyTrader
 {
-    class AsynchTradeStorage : ITradeStorage
-
+    public class AsynchTradeStorage : ITradeStorage
     {
 
+        AdoNetTradeStorage tradeStorage;
+
+        private readonly ILogger logger;
+    
+        public AsynchTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            tradeStorage = new AdoNetTradeStorage(logger);
+        }
+
+  
         public void Persist(IEnumerable<TradeRecord> trades)
         {
-            throw new NotImplementedException();
+            Task.Run(() => tradeStorage.Persist(trades));
+
         }
         //The form should be creating an AsynchTradeStorage Object
 
